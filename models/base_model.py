54392001e1999b3941b7d012c2c53c02e3614c82
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-03-10 22:40:01 +0100

    do count in console.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
 """
-This is a BaseModel class that defines all common attributes or methods for other classes
+BaseModel class that defines all common attributes/methods for other classes
 """
 import uuid
 from datetime import datetime
 import models
+
 
 class BaseModel:
     """
     Defines all common attributes/methods for other classes
     """
 
-
     def __init__(self, **kwargs):
         """
         This is where all the attributes will be defined
         """
         if kwargs:
-                self.id = kwargs.get("id")
-                self.created_at = datetime.fromisoformat(kwargs.get("created_at"))
-                self.updated_at = datetime.fromisoformat(kwargs.get("updated_at"))
-                self.name = kwargs.get("name")
-                self.my_number = kwargs.get("my_number")
+            self.id = kwargs.get("id")
+            a = datetime.fromisoformat(kwargs.get("created_at"))
+            self.created_at = a
+            b = datetime.fromisoformat(kwargs.get("updated_at"))
+            self.updated_at = b
+            self.name = kwargs.get("name")
+            self.my_number = kwargs.get("my_number")
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -43,7 +45,7 @@ class BaseModel:
 
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values of __dict__ of the instance
+        returns dictionary containing all keys/values of __dict__ instance
         """
         di = self.__dict__.copy()
         di["created_at"] = self.created_at.isoformat()
